c6c15ada168122459dcb24c4665c5e83fd9d373d
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-08-13 18:38:50 +0700

    Update ECAPAModel.py

--- ECAPAModel.py
+++ ECAPAModel.py
@@ -100,8 +100,8 @@ class ECAPAModel(nn.Module):
 
 		### SAVE SCORE EMBEDDING
 		print("SAVING EMBEDDING SCORES ....")
-		os.makedirs(output_score_file, exist_ok = True)
-		with open(os.path.join(output_score_file, "output_score_file.pkl"), "wb") as f:
+		os.makedirs(output_score_dir, exist_ok = True)
+		with open(os.path.join(output_score_dir, "output_score_file.pkl"), "wb") as f:
 			pickle.dump(scores,f)
 		print("FINISH SAVING EMBEDDING SCORE!!")
 
@@ -190,6 +190,7 @@ class ECAPAModel(nn.Module):
 				continue
 
 			self_state[name].copy_(param)
+
 
 
 
